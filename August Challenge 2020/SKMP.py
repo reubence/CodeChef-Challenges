492b187bb3c79760ff7b13f6aa5e1d026a61dddd
reubence <reuben3699@gmail.com>
2020-08-09 03:45:18 +0530

    4th Problem-Final

--- August Challenge 2020/SKMP.py
+++ August Challenge 2020/SKMP.py
@@ -4,7 +4,6 @@ substring = []
 for i in range(n):
     string.append(str(input().strip()).lower())
     substring.append(str(input().strip()).lower())
-
 def isAnagram(str1, str2):
     if len(str1) == 1:
         return(print(str1))
@@ -19,44 +18,49 @@ def isAnagram(str1, str2):
                     if ord(new_str[i])<=ord(str2[0]) and ord(new_str[i+1])>ord(str2[0]):
                         return(print(new_str[:i+1] + str2 + new_str[i+1:]))
                         break
+                    elif ord(new_str[i]) > ord(str2[0]):
+                        return(print(str2+new_str))
             except:
                 return(print(new_str + str2))
         else:
-            yos = new_str[:new_str.index(str2[0])] + str2 + new_str[new_str.index(str2[0]):]
-            POS = new_str[:new_str.index(str2[0])+1] + str2 + new_str[new_str.index(str2[0])+1:]
             try:
+                yos = new_str[:new_str.index(str2[0])] + str2 + new_str[new_str.index(str2[0]):]
+                POS = new_str[:new_str.index(str2[0]) + 1] + str2 + new_str[new_str.index(str2[0]) + 1:]
+
                 for i in range(len(new_str)):
-                    # print(i)
                     if ord(new_str[i])<=ord(str2[0]) and ord(new_str[i+1])>ord(str2[0]):
                         return(print(min(new_str[:i+1] + str2 + new_str[i+1:],yos,POS)))
                         break
+                    elif ord(new_str[i]) == ord(str2[0]) and ord(new_str[i + 1]) > ord(str2[0]):
+                        yos2 = str2+new_str
+                        pos2 = new_str[:i]+str2+new_str[i:]
+                        return (print(min(new_str[:i + 1] + str2 + new_str[i + 1:], yos2, pos2)))
+                        break
             except:
-                return(print(min(new_str + str2),yos,POS))
+                return(print(min(new_str + str2,yos,POS)))
 for i,x in zip(string,substring):
     isAnagram(i,x)
 
 
-
-
 #
-from copy import deepcopy
-
-for _ in range(int(input())):
-    s = list(input())
-    p = list(input())
-    for i in p:
-        s.remove(i)
-    s.sort()
-    ne = deepcopy(s)
-    ne.append(p[0])
-    ne = sorted(ne,reverse=True)
-    print(ne)
-    print(s)
-    if p[0] not in s:
-        print(''.join(s[0:len(ne) - ne.index(p[0]) - 1]) +''.join(p) +''.join(s[len(ne) - ne.index(p[0])-1:]))
-    else:
-        air = ''.join(s[0:s.index(p[0])]) + ''.join(p) + ''.join(s[s.index(p[0]):])
-        # print(air)
-        print(min(air,''.join(s[0:len(ne) - ne.index(p[0]) - 1]) +''.join(p) +''.join(s[len(ne) - ne.index(p[0])-1:])))
-
-
+# from copy import deepcopy
+#
+# for _ in range(int(input())):
+#     s = list(input())
+#     p = list(input())
+#     for i in p:
+#         s.remove(i)
+#     s.sort()
+#     ne = deepcopy(s)
+#     ne.append(p[0])
+#     ne = sorted(ne,reverse=True)
+#     print(ne)
+#     print(s)
+#     if p[0] not in s:
+#         print(''.join(s[0:len(ne) - ne.index(p[0]) - 1]) +''.join(p) +''.join(s[len(ne) - ne.index(p[0])-1:]))
+#     else:
+#         air = ''.join(s[0:s.index(p[0])]) + ''.join(p) + ''.join(s[s.index(p[0]):])
+#         # print(air)
+#         print(min(air,''.join(s[0:len(ne) - ne.index(p[0]) - 1]) +''.join(p) +''.join(s[len(ne) - ne.index(p[0])-1:])))
+#
+#
